bd86dcb5b202ff21fad7338e5d168cd3ec36424a
Vaishali <vv1705260@gmail.com>
2026-05-04 21:53:04 +0530

    Create Checkout.js

---
+++ client/src/Pages/Checkout.js
@@ -0,0 +1,11 @@
+export default function Checkout() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Checkout</h1>
+
+      <button className="bg-green-600 text-white px-4 py-2 mt-4 rounded">
+        Place Order
+      </button>
+    </div>
+  );
+}
