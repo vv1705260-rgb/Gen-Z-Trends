467a494edf79c1f07bd9b8740f09c8d5498ee4fd
Vaishali <vv1705260@gmail.com>
2026-05-04 21:44:06 +0530

    Create server.js

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => console.log("Server running"));
