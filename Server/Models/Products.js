5dd47958eaafabd2a04d0fc3dff873f44a824ae2
Vaishali <vv1705260@gmail.com>
2026-05-04 21:46:28 +0530

    Create Products.js

---
+++ Server/Models/Products.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  image: String,
+  category: String,
+  description: String
+});
+
+module.exports = mongoose.model("Product", productSchema);
