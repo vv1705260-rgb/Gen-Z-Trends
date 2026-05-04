dafce6c63749e43f974da49e03091431d92040ee
Vaishali <vv1705260@gmail.com>
2026-05-04 21:49:23 +0530

    Create authRoutes.js

---
+++ Server/Routes/authRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// register
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = await User.create({
+    email: req.body.email,
+    password: hashed
+  });
+
+  res.json(user);
+});
+
+// login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const match = await bcrypt.compare(req.body.password, user.password);
+
+  if (!match) return res.status(400).json("Invalid");
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+});
+
+module.exports = router;
