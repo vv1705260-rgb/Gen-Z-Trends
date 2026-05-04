d30736d68cdd4c04b8eedbfd1898c80a5df47df4
Vaishali <vv1705260@gmail.com>
2026-05-04 21:48:18 +0530

    Create ordersRoutes.js

---
+++ Server/Routes/ordersRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Order = require("../models/Order");
+
+router.post("/", async (req, res) => {
+  const order = await Order.create(req.body);
+  res.json(order);
+});
+
+router.get("/", async (req, res) => {
+  const orders = await Order.find();
+  res.json(orders);
+});
+
+module.exports = router;
