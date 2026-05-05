efabc9340cf420a83977896e796b1a51a14b3671
Vaishali <vv1705260@gmail.com>
2026-05-05 08:00:30 +0530

    Add Navbar and Home components to App

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -17,3 +17,16 @@ const App = () => {
 };
 
 export default App;
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
