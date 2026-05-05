7c7a1fc3bb6c100ba5f50882620a574a924658bf
Vaishali <vv1705260@gmail.com>
2026-05-05 07:43:34 +0530

    Create app.jsx

---
+++ client/src/app.jsx
@@ -0,0 +1,19 @@
+import AppRoutes from "./routes/AppRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-dark text-white">
+      <Navbar />
+      
+      <main className="flex-1">
+        <AppRoutes />
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
